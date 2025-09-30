4cec6694402390faa0b94c372b1998293a85311a
Марк Лендер 241-3211 <lendermark114@gmail.com>
2025-09-30 18:56:57 +0300

    Лабораторная №4

---
+++ lunch/js/dishes-data.js
@@ -0,0 +1,83 @@
+const DISHES = [
+  // Soups
+  {
+    keyword: 'gazpacho',
+    name: 'Гаспачо',
+    price: 195,
+    category: 'soup',
+    count: '350 г',
+    image: 'soups/gazpacho',
+  },
+  {
+    keyword: 'mushroom_soup',
+    name: 'Грибной суп-пюре',
+    price: 185,
+    category: 'soup',
+    count: '330 г',
+    image: 'soups/mushroom_soup',
+  },
+  {
+    keyword: 'norwegian_soup',
+    name: 'Норвежский суп',
+    price: 270,
+    category: 'soup',
+    count: '330 г',
+    image: 'soups/norwegian_soup',
+  },
+
+  // Main courses
+  {
+    keyword: 'fried_potatoes',
+    name: 'Жареная картошка с грибами',
+    price: 150,
+    category: 'main_course',
+    count: '250 г',
+    image: 'main_course/friedpotatoeswithmushrooms1',
+  },
+  {
+    keyword: 'lasagna',
+    name: 'Лазанья',
+    price: 385,
+    category: 'main_course',
+    count: '310 г',
+    image: 'main_course/lasagna',
+  },
+  {
+    keyword: 'chicken_cutlets',
+    name: 'Котлеты из курицы с картофельным пюре',
+    price: 225,
+    category: 'main_course',
+    count: '280 г',
+    image: 'main_course/chickencutletsandmashedpotatoes',
+  },
+
+  // Beverages
+  {
+    keyword: 'orange_juice',
+    name: 'Апельсиновый сок',
+    price: 120,
+    category: 'beverages',
+    count: '300 мл',
+    image: 'beverages/orangejuice',
+  },
+  {
+    keyword: 'apple_juice',
+    name: 'Яблочный сок',
+    price: 90,
+    category: 'beverages',
+    count: '300 мл',
+    image: 'beverages/applejuice',
+  },
+  {
+    keyword: 'carrot_juice',
+    name: 'Морковный сок',
+    price: 110,
+    category: 'beverages',
+    count: '300 мл',
+    image: 'beverages/carrotjuice',
+  },
+];
+
+window.DISHES = DISHES;
+
+
